b187a810ec063f3188319bf81d3826a0048dfd32
htoomik <helen@toomik.net>
2017-12-05 10:05:44 +0100

    Day 3, step 1

--- Code/Day03.cs
+++ Code/Day03.cs
@@ -8,6 +8,96 @@ namespace AoC.Code
 {
     class Day03
     {
-        // pen and paper solution for now
+        public static int Solve(int value)
+        {
+            var data = new Dictionary<Tuple<int, int>, int>();
+            data.Add(new Tuple<int, int>(0, 0), 1);
+            data.Add(new Tuple<int, int>(1, 0), 2);
+            data.Add(new Tuple<int, int>(1, 1), 3);
+            data.Add(new Tuple<int, int>(0, 1), 4);
+            data.Add(new Tuple<int, int>(-1, 1), 5);
+
+            var cursor = new Cursor
+            {
+                X = -1,
+                Y = 1,
+                Direction = Direction.Down
+            };
+            for (var i = 6; i <= value; i++)
+            {
+                Move(data, cursor);
+                //Console.WriteLine($"Value {i} is at ({cursor.X}, {cursor.Y})");
+                data.Add(new Tuple<int, int>(cursor.X, cursor.Y), i);
+            }
+            return Math.Abs(cursor.X) + Math.Abs(cursor.Y);
+        }
+
+
+        private static void Move(Dictionary<Tuple<int, int>, int> grid, Cursor cursor)
+        {
+            var lookAtDirection = cursor.GetTurnDirection();
+            var neighborCoordinates = GetNeighborCoordinates(cursor.X, cursor.Y, lookAtDirection);
+            var hasNeighbor = grid.ContainsKey(neighborCoordinates);
+            var passedCorner = !hasNeighbor;
+
+            if (passedCorner)
+                cursor.Turn();
+            cursor.Step();
+        }
+
+
+        private static Tuple<int, int> GetNeighborCoordinates(int x, int y, Direction direction)
+        {
+            var cursor = new Cursor { X = x, Y = y, Direction = direction };
+            cursor.Step();
+            return new Tuple<int, int>(cursor.X, cursor.Y);
+        }
+
+
+        private class Cursor
+        {
+            public Direction Direction;
+            public int X;
+            public int Y;
+
+            public void Step()
+            {
+                switch (Direction)
+                {
+                    case Direction.Up: Y++; break;
+                    case Direction.Down: Y--; break;
+                    case Direction.Left: X--; break;
+                    case Direction.Right: X++; break;
+                    default: throw new Exception();
+                }
+            }
+
+
+            public void Turn()
+            {
+                Direction = GetTurnDirection();
+            }
+
+
+            public Direction GetTurnDirection()
+            {
+                switch (Direction)
+                {
+                    case Direction.Up: return Direction.Left;
+                    case Direction.Down: return Direction.Right;
+                    case Direction.Left: return Direction.Down;
+                    case Direction.Right: return Direction.Up;
+                    default: throw new Exception();
+                }
+            }
+        }
+
+        private enum Direction
+        {
+            Up,
+            Down,
+            Left,
+            Right
+        }
     }
 }
--- Test/Test03.cs
+++ Test/Test03.cs
@@ -1,13 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AoC.Code;
+using System;
+using NUnit.Framework;
 
 namespace AoC.Test
 {
+    [TestFixture]
     class Test03
     {
-        // pen and paper solution for now
+        [Test]
+        public void Examples1()
+        {
+            Assert.That(Day03.Solve(12), Is.EqualTo(3));
+            Assert.That(Day03.Solve(23), Is.EqualTo(2));
+            Assert.That(Day03.Solve(1024), Is.EqualTo(31));
+        }
+
+
+        [Test]
+        public void Step1()
+        {
+            Console.WriteLine(Day03.Solve(368078));
+        }
     }
 }
