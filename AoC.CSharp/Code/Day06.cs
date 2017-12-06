31eef734129ac128fdc8a06e73e4dd823d1bbb8c
htoomik <helen@toomik.net>
2017-12-06 10:22:00 +0100

    Day 6

---
+++ AoC.CSharp/Code/Day06.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC.Code
+{
+    public class Day06
+    {
+        public static Tuple<int, int> Solve(IEnumerable<int> input)
+        {
+            var counter = 0;
+            var seen = new Dictionary<string, int>();
+            var state = input.ToList();
+
+            var hash = string.Join(",", state);
+            seen.Add(hash, counter);
+
+            while (true)
+            {
+                counter++;
+                state = Redistribute(state);
+                hash = string.Join(",", state);
+                //Console.WriteLine($"Run {counter} - state: {hash}");
+                if (seen.ContainsKey(hash))
+                    break;
+                seen.Add(hash, counter);
+            }
+            return new Tuple<int, int>(counter, counter - seen[hash]);
+        }
+
+
+        private static List<int> Redistribute(List<int> state)
+        {
+            var max = state.Max();
+            var indexOfMax = state.IndexOf(max);
+
+            var newState = new List<int>(state);
+            newState[indexOfMax] = 0;
+
+            var j = indexOfMax + 1;
+            if (j == state.Count)
+                    j = 0;
+
+            for (var i = 0; i < max; i++)
+            {
+                newState[j]++;
+                j++;
+                if (j == state.Count)
+                    j = 0;
+            }
+
+            if (state.Sum() != newState.Sum())
+                throw new Exception("total does not match!");
+
+            return newState;
+        }
+    }
+}
---
+++ AoC.CSharp/Test/Test06.cs
@@ -0,0 +1,28 @@
+﻿using AoC.Code;
+using NUnit.Framework;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace AoC.Test
+{
+    [TestFixture]
+    public class Test06
+    {
+        [Test]
+        public void Examples1()
+        {
+            var result = Day06.Solve(new[] { 0, 2, 7, 0 });
+            Assert.That(result.Item1, Is.EqualTo(5));
+            Assert.That(result.Item2, Is.EqualTo(4));
+        }
+
+
+        [Test]
+        public void Step1()
+        {
+            var data = File.ReadAllText(@"C:\code\aoc 2017\input_06.txt").Trim().Split('\t').Select(s => int.Parse(s));
+            Console.WriteLine(Day06.Solve(data));
+        }
+    }
+}
