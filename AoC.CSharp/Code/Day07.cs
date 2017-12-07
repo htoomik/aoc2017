67bb5e4e4c1b2a99284920d4db6c188addd3a997
htoomik <helen@toomik.net>
2017-12-07 10:39:35 +0100

    feat(day 7)

---
+++ AoC.CSharp/Code/Day07.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC.Code
+{
+    class Day07
+    {
+        public static string Solve(IEnumerable<string> indata)
+        {
+            var nodes = MakeTree(indata);
+            var root = nodes.Single(n => n.IsRoot);
+            return root.Name;
+        }
+
+
+        public static int Solve2(IEnumerable<string> indata)
+        {
+            var nodes = MakeTree(indata);
+            var root = nodes.Single(n => n.IsRoot);
+            root.CalculateWeight();
+            var nodeNotInBalance = nodes
+                .Single(n => !n.ChildrenInBalance &&
+                            n.Children.All(c => c.ChildrenInBalance));
+            var byWeight = nodeNotInBalance.Children.GroupBy(c => c.WeightWithChildren);
+            var wrong = byWeight.Single(g => g.Count() == 1).Key;
+            var right = byWeight.First(g => g.Count() != 1).Key;
+            var diff = right - wrong;
+            var correctWeight = nodeNotInBalance.Children.Single(c => c.WeightWithChildren == wrong).Weight + diff;
+            return correctWeight;
+        }
+
+
+        private static List<Node> MakeTree(IEnumerable<string> indata)
+        {
+            var nodes = indata.Select(row => Parse(row)).ToList();
+            foreach (var node in nodes)
+            {
+                foreach (var childName in node.ChildNames)
+                {
+                    var child = nodes.Single(n => n.Name == childName);
+                    node.Children.Add(child);
+                    child.IsRoot = false;
+                }
+            }
+            if (nodes.Count(n => n.IsRoot) > 1)
+                throw new Exception("More than one root found");
+            return nodes;
+        }
+
+
+        private static Node Parse(string row)
+        {
+            var parts = row.Split(new[] { " -> " }, StringSplitOptions.None);
+            var nameAndWeight = parts[0].Split('(');
+            var name = nameAndWeight[0].Trim();
+            var weight = int.Parse(nameAndWeight[1].Replace(")", "").Trim());
+            var childNames = parts.Length > 1 ? parts[1].Split(',').Select(s => s.Trim()).ToList() : new List<string>();
+            return new Node
+            {
+                Name = name,
+                Weight = weight,
+                ChildNames = childNames
+            };
+        }
+
+        private class Node
+        {
+            public string Name;
+            public int Weight;
+            public List<string> ChildNames;
+            public bool IsRoot = true;
+
+            public List<Node> Children;
+            public int WeightWithChildren;
+            public bool ChildrenInBalance;
+
+
+            public Node()
+            {
+                Children = new List<Node>();
+            }
+
+
+            public void CalculateWeight()
+            {
+                foreach (var c in Children)
+                    c.CalculateWeight();
+
+                var childWeights = Children.Select(c => c.WeightWithChildren);
+                if (childWeights.Any())
+                {
+                    WeightWithChildren = Weight + childWeights.Sum();
+                    ChildrenInBalance = childWeights.Min() == childWeights.Max();
+                }
+                else
+                {
+                    WeightWithChildren = Weight;
+                    ChildrenInBalance = true;
+                }
+            }
+        }
+    }
+}
---
+++ AoC.CSharp/Test/Test07.cs
@@ -0,0 +1,58 @@
+﻿using AoC.Code;
+using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace AoC.Test
+{
+    [TestFixture]
+    class Test07
+    {
+        private static readonly string[] data = new[]
+            {
+                "pbga (66)",
+                "xhth (57)",
+                "ebii (61)",
+                "havc (66)",
+                "ktlj (57)",
+                "fwft (72) -> ktlj, cntj, xhth",
+                "qoyq (66)",
+                "padx (45) -> pbga, havc, qoyq",
+                "tknk (41) -> ugml, padx, fwft",
+                "jptl (61)",
+                "ugml (68) -> gyxo, ebii, jptl",
+                "gyxo (61)",
+                "cntj (57)"
+            };
+
+
+        [Test]
+        public void Examples1()
+        {
+            Assert.That(Day07.Solve(data), Is.EqualTo("tknk"));
+        }
+
+
+        [Test]
+        public void Step1()
+        {
+            var indata = File.ReadAllLines(@"C:\Code\Aoc 2017\input_07.txt");
+            Console.WriteLine(Day07.Solve(indata));
+        }
+
+
+        [Test]
+        public void Examples2()
+        {
+            Assert.That(Day07.Solve2(data), Is.EqualTo(60));
+        }
+
+
+        [Test]
+        public void Step2()
+        {
+            var indata = File.ReadAllLines(@"C:\Code\Aoc 2017\input_07.txt");
+            Console.WriteLine(Day07.Solve2(indata));
+        }
+    }
+}
