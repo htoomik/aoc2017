5ee6ee94941e8fc469a554b1d1f75a5250729a28
htoomik <helen@toomik.net>
2017-12-08 08:57:47 +0100

    feat(day 8)

---
+++ AoC.CSharp/Code/Day08.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC.Code
+{
+    public static class Day08
+    {
+        public static dynamic Solve(IEnumerable<string> input)
+        {
+            var instructions = input.Select(Parse);
+            var registers = new Dictionary<string, int>();
+            var max = 0;
+
+            foreach (var i in instructions)
+            {
+                if (EvaluateCondition(i, registers))
+                {
+                    if (!registers.ContainsKey(i.Register))
+                        registers.Add(i.Register, 0);
+                    var currentValue = registers[i.Register];
+                    var newValue = currentValue + i.Increment;
+                    max = Math.Max(max, newValue);
+                    registers[i.Register] = newValue;
+                }
+            }
+
+            return new
+            {
+                FinalMax = registers.Values.Max(),
+                RunningMax = max
+            };
+        }
+
+
+        private static bool EvaluateCondition(Instruction i, Dictionary<string, int> registers)
+        {
+            var value1 =0 ;
+            registers.TryGetValue(i.ConditionRegister, out value1);
+            var value2 = i.ConditionTerm;
+            switch (i.ConditionOperator)
+            {
+                case "<": return value1 < value2;
+                case ">": return value1 > value2;
+                case "==": return value1 == value2;
+                case ">=": return value1 >= value2;
+                case "<=": return value1 <= value2;
+                case "!=": return value1 != value2;
+                default: throw new Exception("Unexpected condition " + i.ConditionOperator);
+            }
+        }
+
+        private static Instruction Parse(string s)
+        {
+            var parts = s.Split(' ');
+            return new Instruction
+            {
+                Register = parts[0],
+                Operation = parts[1],
+                Term = int.Parse(parts[2]),
+                ConditionRegister = parts[4],
+                ConditionOperator = parts[5],
+                ConditionTerm = int.Parse(parts[6])
+            };
+        }
+
+        private class Instruction
+        {
+            public string Register;
+            public string Operation;
+            public int Term;
+            public string ConditionRegister;
+            public string ConditionOperator;
+            public int ConditionTerm;
+
+            public int Increment
+            {
+                get
+                {
+                    switch (Operation)
+                    {
+                        case "inc": return Term;
+                        case "dec": return -Term;
+                        default: throw new Exception("Unexpected operation " + Operation);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AoC.CSharp/Test/Test08.cs
@@ -0,0 +1,32 @@
+﻿using AoC.Code;
+using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace AoC.Test
+{
+    [TestFixture]
+    public class Test08
+    {
+        [Test]
+        public void Examples()
+        {
+            var input = new[] {
+                "b inc 5 if a > 1",
+                "a inc 1 if b < 5",
+                "c dec -10 if a >= 1",
+                "c inc -20 if c == 10"
+            };
+            Assert.That(Day08.Solve(input).FinalMax, Is.EqualTo(1));
+            Assert.That(Day08.Solve(input).RunningMax, Is.EqualTo(10));
+        }
+
+
+        [Test]
+        public void Step1()
+        {
+            var input = File.ReadAllLines(@"C:\Code\AoC 2017\input_08.txt");
+            Console.WriteLine(Day08.Solve(input));
+        }
+    }
+}
