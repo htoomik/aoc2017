7db232c51dba159bf2cea530f4c17689c27516e1
htoomik <helen@toomik.net>
2017-12-11 10:01:36 +0100

    feat(day11)

---
+++ AoC.CSharp/Code/Day11.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace AoC.Code
+{
+    public class Day11
+    {
+        public static Tuple<int, int> Solve(string indata)
+        {
+            var steps = indata.Split(',');
+
+            int totalX = 0;
+            int totalY = 0;
+            int maxDistance = 0;
+
+            foreach (var step in steps)
+            {
+                var move = Navigate(step);
+                totalX += move.X;
+                totalY += move.Y;
+                var distance = GetDistance(totalX, totalY);
+                maxDistance = Math.Max(maxDistance, distance);
+            }
+            
+            var finalDistance = GetDistance(totalX, totalY);
+            return new Tuple<int, int>(finalDistance, maxDistance);
+        }
+
+
+        private static XY Navigate(string step)
+        {
+            switch (step)
+            {
+                case "n": return new XY(0, 2);
+                case "s": return new XY(0, -2);
+                case "ne": return new XY(1, 1);
+                case "se": return new XY(1, -1);
+                case "nw": return new XY(-1, 1);
+                case "sw": return new XY(-1, -1);
+            }
+            return new XY(0, 0);
+        }
+
+
+        private static int GetDistance(int x, int y)
+        {
+            var xSteps = Math.Abs(x);
+            var ySteps = Math.Abs(y);
+
+            // each step in the grid can be either one x unit and one y unit, or two y units.
+            return xSteps + Math.Abs(ySteps - xSteps) / 2;
+        }
+
+        private class XY
+        {
+            public int X;
+            public int Y;
+
+
+            public XY(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+    }
+}
---
+++ AoC.CSharp/Test/Test11.cs
@@ -0,0 +1,28 @@
+﻿using AoC.Code;
+using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace AoC.Test
+{
+    [TestFixture]
+    public class Test11
+    {
+        [Test]
+        public void Examples1()
+        {
+            Assert.That(Day11.Solve("ne,ne,ne").Item1, Is.EqualTo(3));
+            Assert.That(Day11.Solve("ne,ne,sw,sw").Item1, Is.EqualTo(0));
+            Assert.That(Day11.Solve("ne,ne,s,s").Item1, Is.EqualTo(2));
+            Assert.That(Day11.Solve("se,sw,se,sw,sw").Item1, Is.EqualTo(3));
+        }
+
+
+        [Test]
+        public void Step1()
+        {
+            var indata = File.ReadAllLines(@"C:\Code\AoC 2017\input_11.txt")[0];
+            Console.WriteLine(Day11.Solve(indata));
+        }
+    }
+}
