da660d03a9dee75def0d01569794ea30e13f6f24
htoomik <helen@toomik.net>
2017-12-12 09:31:18 +0100

    feat(day 12)

---
+++ AoC.CSharp/Code/Day12.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC.Code
+{
+    public class Day12
+    {
+        public static Tuple<int, int> Solve(IEnumerable<string> indata)
+        {
+            var groups = indata
+                .Select(s => s.Split(new[] { " <-> " }, StringSplitOptions.None))
+                .Select(p => p[1].Split(',').Select(s => s.Trim()).Union(new[] { p[0] }).ToList()).ToList();
+
+            var count1 = groups.Count();
+            var count2 = 0;
+
+            while (count1 != count2)
+            {
+                count1 = count2;
+                groups = Merge(groups);
+                count2 = groups.Count();
+            }
+
+            var zeroGroup = groups.Single(g => g.Contains("0"));
+            return new Tuple<int, int>(zeroGroup.Count(), count2);
+        }
+
+
+        private static List<List<string>> Merge(List<List<string>> groups)
+        {
+            var result = new List<List<string>>();
+
+            while (groups.Any())
+            {
+                var newGroup = groups.First();
+                var connected = groups.Where(g => g.Intersect(newGroup).Any()).ToList();
+                newGroup.AddRange(connected.SelectMany(g => g).ToList());
+                foreach (var c in connected)
+                    groups.Remove(c);
+
+                result.Add(newGroup.Distinct().ToList());
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AoC.CSharp/Test/Test12.cs
@@ -0,0 +1,35 @@
+﻿using AoC.Code;
+using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace AoC.Test
+{
+    [TestFixture]
+    public class Test12
+    {
+        [Test]
+        public void Example1()
+        {
+            var input = new[] {
+                "0 <-> 2",
+                "1 <-> 1",
+                "2 <-> 0, 3, 4",
+                "3 <-> 2, 4",
+                "4 <-> 2, 3, 6",
+                "5 <-> 6",
+                "6 <-> 4, 5"
+            };
+            Assert.That(Day12.Solve(input).Item1, Is.EqualTo(6));
+            Assert.That(Day12.Solve(input).Item2, Is.EqualTo(2));
+        }
+
+
+        [Test]
+        public void Solve1()
+        {
+            var indata = File.ReadAllLines(@"C:\Code\AoC 2017\input_12.txt");
+            Console.WriteLine(Day12.Solve(indata));
+        }
+    }
+}
